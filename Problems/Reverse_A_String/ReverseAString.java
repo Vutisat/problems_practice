ebeed6260f5f51fbf5a42f69cad14b1914dffbd4
vutisat <vutisat@miamioh.edu>
2021-01-30 06:53:41 -0500

    first prob

---
+++ Problems/Reverse_A_String/ReverseAString.java
@@ -0,0 +1,19 @@
+package Reverse_A_String;
+
+public class ReverseAString {
+
+    public static void main(String[] args) {
+        String input = "ThisStringShouldBeReverse24";
+
+        byte[] strAsByteArray = input.getBytes();
+
+        byte[] result = new byte[strAsByteArray.length];
+
+        //Reverse the bits
+        for (int i = 0; i < strAsByteArray.length; i++){
+            result[i] = strAsByteArray[strAsByteArray.length - i - 1];
+        }
+
+        System.out.println(new String(result));
+    }
+}
